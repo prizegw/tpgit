47184b991457a3575a92119291b8c2363ae072f5
Francis <francis.siegni@groupe-esigelec.org>
2024-02-22 16:05:18 +0100

    nouveau commit Animal

--- src/tpgit/Animal.java
+++ src/tpgit/Animal.java
@@ -1,5 +1,31 @@
 package tpgit;
 
 public class Animal {
+	
+	private String couleur;
+	private Boolean carniv;
+	
+	public Animal(String couleur, Boolean carni) {
+		
+		this.couleur = couleur;
+		this.carniv = carniv;
+	}
 
+	public String getCouleur() {
+		return couleur;
+	}
+
+	public void setCouleur(String couleur) {
+		this.couleur = couleur;
+	}
+
+	public Boolean getCarniv() {
+		return carniv;
+	}
+
+	public void setCarniv(Boolean carniv) {
+		this.carniv = carniv;
+	}
+	
+  
 }
